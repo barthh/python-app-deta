af325568a78b208b61098d4ea9ae8906f2a2dc1f
Barthh <barthelemy.popille@epfedu.fr>
2022-12-07 15:14:07 +0100

    Google analytics tracking

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-def app(event):
-    return "Hellow world!"
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5W5P1Y2GKG"></script> <script>
+    window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
+    gtag('config', ' G-5W5P1Y2GKG'); </script>
+    """
+    return prefix_google + "HelloW World"
